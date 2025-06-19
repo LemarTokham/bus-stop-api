76ece92201320140d6312c52e4daf1862c903e35
Abhi <abhiram.bhat2005@gmail.com>
2025-06-19 18:30:31 +0100

    Lists bus numbers of merseyside
    Created venv, use requests to get all bus numbers in merseyside using API

--- app.py
+++ app.py
@@ -1,21 +1,46 @@
-import json
-from flask import Flask, jsonify, request
+import json, requests as request
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from flask import Flask, jsonify
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hi mom"
+global API_KEY, DATASET_ID
+API_KEY = "b7085c08b7908af68ad10f79008c6c91b88ce318"
+DATASET_ID = "15954"
 
 
-# Busstop data
-@app.route('/api/buses/<atco_code>')
-def get_buses(atco_code):
-    # atco codes representing bus stops, with their values being the buses that stop at them
-    obj = {'2800S42048C':[699],
-           '2800S42053B':[6, 7, 79]
-           }
-    return jsonify(obj[atco_code])
+# get list of all bus numbers
+def get_bus_numbers():
+    url = f"https://data.bus-data.dft.gov.uk/api/v1/dataset/{DATASET_ID}/?api_key={API_KEY}" 
+    response = request.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        bus_numbers = [number for number in data['lines']]
+        print(bus_numbers)
+        return bus_numbers
+    else:
+        print(f"Error: {response.status_code}")
+        return []
 
-if __name__ == "__main__":
-    app.run(debug=True)
+get_bus_numbers()
+
+# import json
+# from flask import Flask, jsonify, request
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "Hi mom"
+
+
+# # Busstop data
+# @app.route('/api/buses/<atco_code>')
+# def get_buses(atco_code):
+#     # atco codes representing bus stops, with their values being the buses that stop at them
+#     obj = {'2800S42048C':[699],
+#            '2800S42053B':[6, 7, 79]
+#            }
+#     return jsonify(obj[atco_code])
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
