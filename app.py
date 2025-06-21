052a4564b9cc4e33ab804d95d4ea3d42b16ded41
Abhi <abhiram.bhat2005@gmail.com>
2025-06-21 12:27:51 +0100

    rewrote app.py to act as main file
    Moved code into their own seperate files that handle a part of the api, would be calling the functions in app.py

--- app.py
+++ app.py
@@ -1,49 +1,26 @@
 import json, requests as request
-from fastapi import FastAPI
+import xml.etree.ElementTree as ET
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from flask import Flask, jsonify
 
-global API_KEY, DATASET_ID
-API_KEY = "b7085c08b7908af68ad10f79008c6c91b88ce318"
-DATASET_ID = "15954"
+from bus_stops import get_bus_stops
+from bus_num import get_bus_numbers
 
 
-# get list of all bus numbers
-def get_bus_numbers():
-    url = f"https://data.bus-data.dft.gov.uk/api/v1/dataset/{DATASET_ID}/?api_key={API_KEY}" 
-    response = request.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        bus_numbers = [number for number in data['lines']]
-        print(bus_numbers)
-        return bus_numbers
-    else:
-        print(f"Error: {response.status_code}")
-        return []
-
-get_bus_numbers()
+def main():
+    # Get bus stops
+    bus_stops_file = 'timetables.json'
+    bus_stops = get_bus_stops(bus_stops_file)
+    
+    # Print each stop one by one for route 7
+    for stop_ref, details in bus_stops.items():
+        print(f"Stop Ref: {stop_ref}, Name: {details['name']}, Coordinates: {details['coordinates']}")
+    
+    # Get bus numbers
+    bus_numbers = get_bus_numbers()
+    print("Bus Numbers:", bus_numbers)
 
 
-
-
-# import json
-# from flask import Flask, jsonify, request
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return "Hi mom"
-
-
-# # Busstop data
-# @app.route('/api/buses/<atco_code>')
-# def get_buses(atco_code):
-#     # atco codes representing bus stops, with their values being the buses that stop at them
-#     obj = {'2800S42048C':[699],
-#            '2800S42053B':[6, 7, 79]
-#            }
-#     return jsonify(obj[atco_code])
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    main()
